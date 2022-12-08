9b3f3c570b9bf7c5cf0b983d4f6d53c53843c852
James Parkhurst <james.parkhurst@diamond.ac.uk>
2022-12-08 10:42:08 +0000

    Added package data

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ def main():
     setup(
         package_dir={"": "src"},
         packages=find_packages(where="src"),
+        package_data={"crystallise": ["src/crystallise/data/elements.json"]},
         install_requires=[
             "gemmi",
             "mendeleev",
